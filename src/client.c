12ab203d9fd84dd620231aa8995b6e1f93148bc1
Igor Fernandes <igorv1286@gmail.com>
2021-10-11 15:57:43 +0200

    update and a little progress

--- src/client.c
+++ src/client.c
@@ -6,13 +6,30 @@
 /*   By: igvaz-fe <igvaz-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 14:07:22 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/10/11 15:23:16 by igvaz-fe         ###   ########.fr       */
+/*   Updated: 2021/10/11 15:52:34 by igvaz-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	pid_t	pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Invalid format. Try: ./client + PID + Message\n");
+		return (1);
+	}
+	else
+	{
+		pid = ft_atoi(argv[1]);
+		if (pid <= 1)
+		{
+			ft_printf("Invalid PID.\n");
+			return (1);
+		}
+		kill(pid, SIGUSR1);
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,13 +6,26 @@
 /*   By: igvaz-fe <igvaz-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 14:07:41 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/10/11 15:03:06 by igvaz-fe         ###   ########.fr       */
+/*   Updated: 2021/10/11 15:53:35 by igvaz-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
+void my_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		ft_printf("Received SIGUSR1!\n");
+}
+
 int	main(void)
 {
+	pid_t	pid;
+
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	signal(SIGUSR1, my_handler);
+	while (1)
+		pause();
 	return (0);
 }
