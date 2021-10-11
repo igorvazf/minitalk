eaa1807ce310c1bb9e1ff77489801fae5dcd7944
Igor Fernandes <igorv1286@gmail.com>
2021-10-11 23:04:45 +0200

    update minitalk

--- src/client.c
+++ src/client.c
@@ -6,11 +6,57 @@
 /*   By: igvaz-fe <igvaz-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 14:07:22 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/10/11 15:52:34 by igvaz-fe         ###   ########.fr       */
+/*   Updated: 2021/10/11 22:52:01 by igvaz-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
+
+static void	receive_confirmation(int signum)
+{
+	if (signum == SIGUSR1)
+	{
+		ft_printf("Signal received!\n");
+		exit(0);
+	}
+}
+
+static void	decimal_conversion(char c, int flip_counter, int pid)
+{
+	if (flip_counter > 0)
+		decimal_conversion(c / 2, flip_counter - 1, pid);
+	if ((c % 2) == 1)
+	{
+		if (kill(pid, SIGUSR1) != 0)
+		{
+			ft_printf("Error signal!\n");
+			exit(1);
+		}
+	}
+	else
+	{
+		if (kill(pid, SIGUSR2) != 0)
+		{
+			ft_printf("Error signal!\n");
+			exit(1);
+		}
+	}
+	usleep(1000);
+}
+
+static void	send_message(int server_pid, char *message)
+{
+	int	i;
+
+	i = 0;
+	while (message[i])
+	{
+		decimal_conversion(message[i], 7, server_pid);
+		i++;
+	}
+	decimal_conversion('\n', 7, server_pid);
+	decimal_conversion(message[i], 7, server_pid);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,15 +67,15 @@ int	main(int argc, char *argv[])
 		ft_printf("Invalid format. Try: ./client + PID + Message\n");
 		return (1);
 	}
-	else
+	pid = ft_atoi(argv[1]);
+	if (pid <= 1)
 	{
-		pid = ft_atoi(argv[1]);
-		if (pid <= 1)
-		{
-			ft_printf("Invalid PID.\n");
-			return (1);
-		}
-		kill(pid, SIGUSR1);
+		ft_printf("Invalid PID.\n");
+		return (1);
 	}
+	signal(SIGUSR1, receive_confirmation);
+	send_message(pid, argv[2]);
+	while (1)
+		pause();
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,25 +6,45 @@
 /*   By: igvaz-fe <igvaz-fe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 14:07:41 by igvaz-fe          #+#    #+#             */
-/*   Updated: 2021/10/11 15:53:35 by igvaz-fe         ###   ########.fr       */
+/*   Updated: 2021/10/11 23:01:08 by igvaz-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-void my_handler(int signum)
+static void	receive_message(int signum, siginfo_t *siginfo, void *unused)
 {
+	static int	ascii;
+	static int	flip_counter;
+
+	ascii = 0;
+	flip_counter = 0;
+	(void)unused;
 	if (signum == SIGUSR1)
-		ft_printf("Received SIGUSR1!\n");
+		ascii += 1 << (7 - flip_counter);
+	flip_counter++;
+	if (flip_counter == 8)
+	{
+		ft_printf("%c", ascii);
+		flip_counter = 0;
+		if (ascii == '\0')
+			if (kill(siginfo->si_pid, SIGUSR1) != 0)
+				ft_printf("Error signal!\n");
+		ascii = 0;
+	}
 }
 
 int	main(void)
 {
-	pid_t	pid;
+	struct sigaction	act;
+	pid_t				pid;
 
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = receive_message;
 	pid = getpid();
-	ft_printf("PID: %d\n", pid);
-	signal(SIGUSR1, my_handler);
+	ft_printf("Server PID: %d\n", pid);
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
 	while (1)
 		pause();
 	return (0);
